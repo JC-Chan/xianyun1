22281f8d90e9ab752e920f2f1e3fef72ae18952f
JC-Chan <1446822779@qq.com>
2019-12-05 15:56:06 +0800

    首页轮播图完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,54 @@
 <template>
-  <div>首页</div>
+  <div class="container">
+    <!-- 幻灯片 -->
+    <el-carousel :interval="5000" arrow="always">
+      <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <div
+          class="banner-image"
+          :style="`
+                background:url(${$axios.defaults.baseURL + item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      // 轮播图数据
+      banners: []
+    };
+  },
+  mounted() {
+    //挂载完毕的时候获取数据渲染轮播图
+    //nuxt 实际上已经将 axios 封装到了 vue 实例的 $axios 当中可以直接使用
+    this.$axios({
+      url: "/scenics/banners"
+    }).then(res => {
+      const { data } = res.data;
+      this.banners = data;
+    });
+  }
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
 
+  /deep/ .el-carousel__container {
+    height: 700px;
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
