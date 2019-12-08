6a6b61bc8cef3ef36d96e910c751d91048d9f53e
JC-Chan <1446822779@qq.com>
2019-12-08 20:54:04 +0800

    渲染机票列表数据

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -10,7 +10,10 @@
         <FlightsListHead />
 
         <!-- 航班信息 -->
-        <div></div>
+        <div>
+          <!-- 航班列表 -->
+          <FlightsItem v-for="(item,index) in dataList" :key="index" :data="item" />
+        </div>
       </div>
 
       <!-- 侧边栏 -->
@@ -23,13 +26,35 @@
 
 <script>
 import FlightsListHead from "@/components/air/flightsListHead";
+import FlightsItem from "@/components/air/flightsItem";
 
 export default {
   data() {
-    return {};
+    return {
+      //航班总数据
+      flightsData: {},
+      //航班列表数据,用于循环 flightsItem 组件,单独出来是因为要分页
+      dataList: []
+    };
   },
   components: {
-    FlightsListHead
+    FlightsListHead,
+    FlightsItem
+  },
+  methods: {
+    //获取航班总数据
+    getData() {
+      this.$axios({
+        url: `airs`,
+        params: this.$route.query //来自 URL 的5个参数
+      }).then(res => {
+        this.flightsData = res.data;
+        this.dataList = this.flightsData.flights;
+      });
+    }
+  },
+  mounted() {
+    this.getData();
   }
 };
 </script>
